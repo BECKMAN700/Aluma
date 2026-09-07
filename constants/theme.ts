6480a6f6fb607e822b639e4208da8a91e50cdec8
Acgsop <acgsop@gmail.com>
2026-09-07 19:11:14 -0300

    feat(ui): adapta cores do tema para identidade Aluma (#10)

--- constants/theme.ts
+++ constants/theme.ts
@@ -5,8 +5,8 @@
 
 import { Platform } from 'react-native';
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#F5A623';
+const tintColorDark = '#FFD54F';
 
 export const Colors = {
   light: {
